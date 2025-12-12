3dbe9342e2536f4b3bff55c39b58ead456e7cc6e
DAVID HOUSHANGI <davidhoushangi@yahoo.com>
2025-12-12 19:54:58 +0300

    Update banking_system_impl.py

--- starter_code/banking_system_impl.py
+++ starter_code/banking_system_impl.py
@@ -371,7 +371,7 @@ class BankingSystemImpl(BankingSystem):
             return None
         
         if self.accounts[account_id]["account_created"] > time_at:
-            break
+            return None 
         
         # apply cashback if needed
         
@@ -383,6 +383,7 @@ class BankingSystemImpl(BankingSystem):
             return 0  # no account activity since creation
         else:
             return self.accounts[account_id]["balance"][max(time_at_or_earlier_timestamps)]
+
 
 
 
